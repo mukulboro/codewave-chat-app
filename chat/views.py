9118e6bb4442055110c2b75d7d002c48c20e45af
BigyaDhungana <bigyadhungana@gmail.com>
2024-09-19 00:35:30 +0545

    created dashboard nav

--- chat/views.py
+++ chat/views.py
@@ -14,3 +14,6 @@ def chat(request):
 def specific_chat(request, chat_id):
     print(chat_id)
     return render(request, "temp_chat.html")
+
+def dashboard(request):
+    return render(request, "dashboard.html")
