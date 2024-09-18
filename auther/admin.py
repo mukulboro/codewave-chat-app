6eca63af0a67ee67e4ade476d468ec63bac3e415
Mukul <mukul.development@gmail.com>
2024-09-18 13:31:08 +0545

    add models

--- auther/admin.py
+++ auther/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import PrivateUser, PublicUser, UserInterests
+
+class PrivateUserAdmin(UserAdmin):
+       fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ()}), 
+    )
+       add_fieldsets = UserAdmin.add_fieldsets + (
+            (None, {'fields': ()}), 
+        )
+       
+@admin.register(PublicUser, UserInterests)
+class UniversalAdmin(admin.ModelAdmin):
+    def get_list_display(self, request):
+        return [field.name for field in self.model._meta.concrete_fields]
+
+admin.site.register(PrivateUser, PrivateUserAdmin)
 
 # Register your models here.
--- auther/models.py
+++ auther/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class PrivateUser(AbstractUser):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    username = models.CharField(max_length=33, unique=True) # To preserve superuser
+    # ^ This field will store a secret token for Google Auth QR
+    email = models.EmailField(max_length=256, unique=True)
+    age = models.IntegerField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'age', 'username']
+
+class PublicUser(models.Model):
+    user = models.OneToOneField(PrivateUser, on_delete=models.CASCADE)
+    location = models.CharField(max_length=52)
+    gender = models.CharField(max_length=25)
+    bio = models.CharField(max_length=256)
+
+
+class UserInterests(models.Model):
+    interest = models.CharField(max_length=32)
+    user = models.ForeignKey(PublicUser, on_delete=models.CASCADE)
+
+
